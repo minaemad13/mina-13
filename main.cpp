41c6842ed6fd9bda456ba77b0a1fc448a8def4ca
minaemad13 <36317499+minaemad13@users.noreply.github.com>
2018-02-11 01:01:27 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int stand[20]={1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0};
+    int p1,p2,p3,p4,i;
+    while (i<20){
+        cin>>p1>>p2;
+        if(p1||p2==stand[i]){
+            stand[i]="-";
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
