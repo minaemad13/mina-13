e4a75ebbaccc9fc3b9f20d80988cec1c4ca4449b
mina emad <36317499+minaemad13@users.noreply.github.com>
2018-11-17 14:59:49 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,78 +1,56 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-int main()
+bool isfound(string sub)
 {
-    cout<<"\t\t\t\t\t\t welcom in my game\n\t\t\t\t\t\t   kayles game";
-    string y= "12345678901234567890";
-    int x=0,a,player_1,player_2;
-
-     while(x<20)
-     {
-        cout<<"\nplayer 1";
-        cout<<"\nhow many numbers you want to choose 1 or 2?";
-        cin>>a;
-
-        if (a==1)
-        {
-            cout<<"choose 1 number from"<<y;
-            cout<<"==>";
-            cin>>player_1;
-            y[player_1-1]='_';
-            x++;
-            cout<<y;
-        }
-        else
-        {
-
-            cout<<"choose 2 number from "<<y;
-            cout<<"==>";
-            cin>>player_1;
-            y[player_1-1]='_';
-            cout<<"==>";
-            cin>>player_1;
-            y[player_1-1]='_';
-            x=x+2;
-            cout<<y;
-        }
-        if(x==20)
-        {
-            cout<<"\n\t\tpalyer 2 is loser\n\t\t   game over :(";
-                break;
-        }
-        cout<<"\nplayer 2\n how manu numbers you want to choose 1 or 2?!";
-        cin>>a;
-        if(a==1)
-        {
-            cout<<"choose 1 number from"<<y;
-            cout<<"==>";
-            cin>>player_2;
-            y[player_2-1]='_';
-            x++;
-            cout<<y;
-        }
-        else
-        {
-            cout<<"choose 2 number from"<<y;
-            cout<<"==>";
-            cin>>player_2;
-            y[player_2-1]='_';
-            cin>>player_2;
-            y[player_2-1]='_';
-            x=x+2;
-            cout<<y;
-        }
-        if(x==20)
-        {
-            cout<<"\n\t\tplaye 1 is loser\n\t\t   game over :(";
-              break;
-     }
-
-
-
+    ifstream file ;
+    file.open("dictionaryEdit.txt",ios::in);
+    if (file.fail())
+    {
+        cout<<"the file cant be opened"<<endl;
+        return false;
     }
 
+    bool a = false;
+    string temp;
+
+    while ( !file.eof() )
+    {
+        string temp;
+        getline( file , temp );
+        if( temp == sub )
+        {
+            return true;
+        }
+    }
+    return a;
+}
+
+void sub_string( string x, int y = 0, string sub = "" )
+{
+    if ( y == x.length() )
+    {
+        if( isfound(sub) == true )
+        {
+            cout<<sub<<endl;
+        }
+
+    }
+    else
+    {
+        sub_string( x, y+1, sub         );
+        sub_string( x, y+1, sub + x[y]  );
+    }
+}
+
+int main()
+{
+    string word;
+    cout<<"enter the word : "<< endl;
+    cin>>word;
+    sub_string( word );
 
     return 0;
 }
